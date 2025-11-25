09ab188b5761fb149561b21e0ea7486a3dbac8f5
Saezhd1 <saezhd1@gmail.com>
2025-11-25 04:18:23 +0000

    Refactor calcular_nocturnidad_por_dia function

--- nocturnidad_app/src/nocturnidad.py
+++ nocturnidad_app/src/nocturnidad.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 
 def calcular_nocturnidad_por_dia(registros):
-    """
-    Calcula minutos nocturnos e importe por día según HI/HF.
-    """
     resultados = []
     for r in registros:
         try:
@@ -13,8 +10,6 @@ def calcular_nocturnidad_por_dia(registros):
             continue
 
         minutos_nocturnos = 0
-
-        # Tramos nocturnos: 22:00–00:59 y 04:00–06:00
         tramos = [
             (datetime.strptime("22:00", "%H:%M"), datetime.strptime("23:59", "%H:%M")),
             (datetime.strptime("00:00", "%H:%M"), datetime.strptime("00:59", "%H:%M")),
@@ -28,18 +23,13 @@ def calcular_nocturnidad_por_dia(registros):
                 if overlap_start < overlap_end:
                     minutos_nocturnos += int((overlap_end - overlap_start).total_seconds() / 60)
 
-        # Tarifa: 0.05 €/min hasta 25/04/2025, luego 0.062 €/min
         fecha = r["fecha"]
         try:
             fecha_dt = datetime.strptime(fecha, "%d/%m/%Y")
         except Exception:
             fecha_dt = datetime.today()
 
-        if fecha_dt <= datetime(2025, 4, 25):
-            tarifa = 0.05
-        else:
-            tarifa = 0.062
-
+        tarifa = 0.05 if fecha_dt <= datetime(2025, 4, 25) else 0.062
         importe = minutos_nocturnos * tarifa
 
         resultados.append({
